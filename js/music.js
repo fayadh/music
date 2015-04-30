522f57ea0c52815c9bdeafe20bd395889dab1865
fayadh <fayadh.almosawi@gmail.com>
2015-04-30 15:22:41 -0400

    Fixed everything

--- js/music.js
+++ js/music.js
@@ -11,61 +11,49 @@ var melodicMinorGoingDown = ['W', 'W', 'H', 'W', 'W', 'H', 'W'];
 var dorianMode = ['W', 'H', 'W', 'W', 'W', 'H', 'W'];
 // var mixolydianMode = [];
 
-function scaleFinder(scale) {
-	var i = 0;
-	var j = 0;
+function scaleFinder(scale, root) {
+	var i = 0
+	// var root = 0; //root sets the root note. 
 	var result = [];
 	while(i < scale.length) {
 		if (scale[i] == 'W') {
-			j += 2;
-			result[i] = j; 
+			root += 2;
+			result[i] = root; 
 		}
 		else if (scale[i] == 'H') {
-	 		j += 1;
-	 		result[i] = j; 
+	 		root += 1;
+	 		result[i] = root; 
 		};
 		i++;
 	};
 	return result;
 };
 
-function scaleIndexToNotes(indexScale) {
+function scaleIndexToNotes(input) {
  result = [];
- for(var i = 0; i < indexScale.length; i++) {
- 	var index = indexScale[i] - 1;
+ for(var i = 0; i < input.length; i++) {
+ 	var index = input[i] - 1;  //
 
  	// Octave recognition.
- 	if (indexScale[i] <= 11) {
+ 	if (input[i] <= 11) {
  		var octave = 0; 
  		result[i] = notes[ index ] + octave; 
  	} else {
- 		var correction = (indexScale[i]) % 11;
- 		var octave = Math.floor( (indexScale[i]) / 11) 
- 		result[i] = notes[ correction ] + octave; //to correct the notes.
+ 			if (((input[i]) % 11) == 0) {
+ 		 		var correction = 10; 
+ 		 	} else {
+ 		 		var correction = ((input[i]) % 11) - 1; 
+ 		 	};
+ 			var octave = Math.floor( (input[i]) / 11) 
+ 			result[i] = notes[ correction ] + octave; //to correct the notes.
  	};
-  
  }
-
  return result;
 } 
 
-// // Now what we want is if the loop hits something after 11,
-// it automatically 	
-
-// Math.floor() <-- Gives us the octave.
-// % gives us the index. 
-
-// First, let's give it octaves. 
 
 $( document ).ready(function() {
-			console.log( scaleFinder(majorScale) );
-      console.log( scaleIndexToNotes(scaleFinder(majorScale)) );
+			experiment = scaleFinder(majorScale, 25)
+			console.log( experiment );
+      console.log( scaleIndexToNotes(experiment));
     });
-
-// scaleFinder(majorScale);
-
-// [1,2,3,4,5,6,7,8,9,10,11]
-// [12,13,14,15,16,17,18,19,20,21]
-// if scale
-
-
